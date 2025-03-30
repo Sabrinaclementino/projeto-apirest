ef3f1baab96682a09f48e26b9f087d9c0f5393fc
Sabrina Clementino <sabrinasantos783@outlook.com>
2025-03-29 22:32:06 -0300

    feat(pages): Lista Produto; Instalação DataGrid

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import Header from "./components/Header";
 import CadastroProduto from "./pages/Produto/CadastroProduto";
+import ListaProduto from "./pages/Produto/ListaProduto";
 
 const App = () => {
   return (
     <div className="App">
       <Header />;
+      <ListaProduto />
       <CadastroProduto />
     </div>
   );
