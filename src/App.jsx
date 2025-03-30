ce05874a13d03ed47074e31faa6f361766e292e5
Sabrina Clementino <sabrinasantos783@outlook.com>
2025-03-29 21:47:59 -0300

    feat(pages): Cadastro Produto

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import Header from "./components/Header";
+import CadastroProduto from "./pages/Produto/CadastroProduto";
 
 const App = () => {
   return (
     <div className="App">
       <Header />;
+      <CadastroProduto />
     </div>
   );
 };
